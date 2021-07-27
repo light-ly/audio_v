a51aba49efc7a605f90f1609ff6c660902c157a7
liuyi <1505507717@qq.com>
2021-07-27 17:13:11 +0800

    Update audio.js

--- audio.js
+++ audio.js
@@ -5,7 +5,7 @@ var screenHeight = document.documentElement.clientHeight,
 var width = canvas.width,
   height = canvas.height;
 
-var audio = new Audio("祖娅纳惜 - 千秋梦.mp4");
+var audio = new Audio("dream.mp4");
 
 audio.oncanplaythrough = function () {
   if (screenWidth != width || screenHeight != height) {
