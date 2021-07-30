6180c63981a277c50371af55ffaa0cfed6753bff
liuyi <1505507717@qq.com>
2021-07-31 02:32:38 +0800

    Update audio.js
    try analyzer time

--- audio.js
+++ audio.js
@@ -192,7 +192,7 @@ function init() {
 function draw() {
     entropy += 1;
     chipAnalyzer.getByteFrequencyData(chipArray);
-    if (entropy > 30 && isDraw) {
+    if (entropy > 20 && isDraw) {
         img_analyzer.src = remoteURL + "?analyzer=" + chipArray;
         entropy = 0;
     }
